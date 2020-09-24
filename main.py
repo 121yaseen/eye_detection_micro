eb704747c81e9db06f20a9daa1ef3e7b7941d5a2
Yaseen <121yaseen@gmail.com>
2020-09-24 15:06:07 +0530

    Main Code Completed

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
 img = cv2.imread('groupfie.jpg')
-img = cv2.resize(500, 500)
+img = cv2.resize(img, (500, 500))
 copy = img.copy()
-gray = cv2.
+gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+eyes = cascade.detectMultiScale(gray, 1.3, 5)
+
+for (ex, ey, ew, eh) in eyes:
+    cv2.rectangle(copy, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+
+stack = np.hstack([img, copy])
+cv2.imshow('Output', stack)
+cv2.waitKey(0)
